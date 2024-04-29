d35b84d5a2639c9fe058cf99ad7dff91464c7780
Vincent_Clifford <vincent.d.clifford@gmail.com>
2024-04-29 10:11:54 +0200

    Add script for setting up environment and running main function

---
+++ run_trajectory_planning.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#This script will set up the correct python evironment and will run the trajectory planning 
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+export PYTHONPATH="$SCRIPT_DIR:$PYTHONPATH"
+echo "The pythonpath has been set to: $PYTHONPATH"
+
+if [[ ":$PYTHONPATH:" != *"/Deterministic_Annealing:"* ]]; then
+    echo "Error: No entry in PYTHONPATH ending with /Deterministic_Annealing. Aborting script."
+    exit 1
+else 
+    echo "Success: PYTHONPATH has been correctly set"
+fi
+
+python3 main.py
+exit 0 
